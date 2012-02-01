28e0253932a9f31fe0ec66573b97fa795e91f870
Alabê Duarte <alabeduarte@gmail.com>
2012-02-01 00:24:21 -0300

    config mongodb on heroku

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,9 +1,4 @@
-# MongoMapper.database = "app-#{Rails.env}"
-MongoMapper.config = { 
-  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 
-                          'mongodb://localhost/selecaotricolor' } }
-
-MongoMapper.connect(Rails.env)
+MongoMapper.database = "app-#{Rails.env}"
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|

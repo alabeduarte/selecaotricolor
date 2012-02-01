8776e62a25e62d94fcc99caee5c5fcd0c2700ffc
Alabê Duarte <alabeduarte@gmail.com>
2012-02-01 00:05:32 -0300

    config mongodb

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,4 +1,9 @@
-MongoMapper.database = "app-#{Rails.env}"
+# MongoMapper.database = "app-#{Rails.env}"
+MongoMapper.config = { 
+  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 
+                          'mongodb://localhost/selecaotricolor' } }
+
+MongoMapper.connect(Rails.env)
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|

f102adc05589f6f528ffd04fe99006195dc26009
Alabê Duarte <alabeduarte@gmail.com>
2012-02-14 21:29:10 -0200

    ..

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,30 +1,30 @@
 class User
   include MongoMapper::Document
+  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   key :nickname, String
+  validates :nickname, :presence => true
 
-   def self.new_with_session(params, session)
-     super.tap do |user|
-       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-         user.email = data["email"]
-       end
-     end
-   end
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"]
+      end
+    end
+  end
 
-   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
-     data = access_token.extra.raw_info
-     if user = User.where(:email => data.email).first
-       user
-     else # Create a user with a stub password. 
-       User.create!(
-                    nickname: data.name,
-                    email: data.email, 
+  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
+    data = access_token.extra.raw_info
+    if user = User.where(:email => data.email).first
+      user
+    else # Create a user with a stub password. 
+      User.create!(nickname: data.name, email: data.email, 
                     password: Devise.friendly_token[0,20]) 
-     end
-   end
+    end
+  end
 
 end

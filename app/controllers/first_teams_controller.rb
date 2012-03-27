a3dca9e5439918b5506a162fbe4c336c2b948068
Alabê Duarte <alabeduarte@gmail.com>
2012-03-26 22:28:37 -0300

    create index page

--- app/controllers/first_teams_controller.rb
+++ app/controllers/first_teams_controller.rb
@@ -4,10 +4,21 @@ class FirstTeamsController < ApplicationController
     @matches = Calendar.with_tactics
   end
   
-  def create    
+  def create
+    FirstTeam.create_from(data: params[:_json], owner: current_user)   
   end
   
-  def newly_created
+  def show
+  end
+  
+  def index
+    @first_teams = FirstTeam.all
+    
+    respond_to do |format|
+      format.html
+      format.xml  { render :xml => @first_teams }
+      format.json  { render :json => @first_teams }
+    end
   end
   
 end

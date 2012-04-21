ed195a15abba139fe7598cec63121423c706c0e9
Alabê Duarte <alabeduarte@gmail.com>
2012-04-21 10:02:32 -0300

    testing when first team is updated

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,8 @@ class WelcomeController < ApplicationController
   def index
     @users = User.top_scorers_of 5
     first_team = FirstTeam.last_of_the_round
-    @recent_winners = first_team.squad_winners_of_the_round(Calendar.last_match)
+    last_match = first_team.formation.match
+    @recent_winners = first_team.squad_winners_of_the_round(last_match)
     @formation = first_team.formation
     @players_positions = @formation.players_ordered_by_positions
   end
--- spec/models/first_team_spec.rb
+++ spec/models/first_team_spec.rb
@@ -7,6 +7,12 @@ describe FirstTeam do
   let(:scorer) { mock(:scorer).as_null_object }
   let(:formation) { mock_model(Formation).as_null_object }
   let(:new_first_team) { FirstTeam.new(formation: formation) }
+  let(:current_user) { Factory(:admin) }
+
+  before(:each) do      
+    Formation::Helper.new      
+    @json_442 = '[ { "formation": {          "player": "4f03b5b6e1af8003be000026"         ,          "x": "0"         ,          "y": "2"     }  }  ,  { "formation": {          "player": "4f25cdcbe1af800323000b46"         ,          "x": "1"         ,          "y": "3"     }  }  ,  { "formation": {          "player": "4f2fd315b3e30f0001000a07"         ,          "x": "2"         ,          "y": "1"     }  }  ,  { "formation": {          "player": "4f04e6d3e1af80017c0000a7"         ,          "x": "2"         ,          "y": "4"     }  }  ,  { "formation": {          "player": "4f04e6b5e1af80017c000073"         ,          "x": "4"         ,          "y": "1"     }  }  ,  { "formation": {          "player": "4f25cc85e1af8003230009be"         ,          "x": "4"         ,          "y": "3"     }  }  ,  { "formation": {          "player": "4f25ca54e1af8003230008b5"         ,          "x": "6"         ,          "y": "0"     }  }  ,  { "formation": {          "player": "4f25ca2de1af800323000896"         ,          "x": "6"         ,          "y": "4"     }  }  ,  { "formation": {          "player": "4f04e6dde1af80017c0000c4"         ,          "x": "7"         ,          "y": "1"     }  }  ,  { "formation": {          "player": "4f2efa0ae1af800c040009c4"         ,          "x": "7"         ,          "y": "3"     }  }  ,  { "formation": {          "player": "4f25c920e1af800323000879"         ,          "x": "-1"         ,          "y": "-1"     }  }  ]'      
+  end
   
   context "scoring players" do
     it "should make reference with scorer before apply scores" do
@@ -29,5 +35,30 @@ describe FirstTeam do
     end
   end
   
-  # context ""
+  context "showing the last round" do
+    it "when the official team has never been created then the first team the last round should be null" do
+      FirstTeam.last_of_the_round.should be_nil
+    end
+    
+    it "should load the first team of the last round while the current match has not been started" do
+      first_team = FirstTeam.new(formation: new_formation)
+      first_team.save
+      FirstTeam.last_of_the_round.id.should == first_team.id
+    end
+    
+    it "should load a new first team when a new was created" do
+      old_first_team = FirstTeam.new(formation: new_formation(@json_442, Calendar.last_match))
+      old_first_team.save
+      FirstTeam.last_of_the_round.id.should == old_first_team.id
+      
+      new_first_team = FirstTeam.new(formation: new_formation)
+      new_first_team.save
+      FirstTeam.last_of_the_round.id.should == new_first_team.id
+    end
+  end
+  
+private
+  def new_formation(json=@json_442, match=Calendar.next_match)
+    Formation.new_by(data: JSON.load(json), owner: current_user, match: match)
+  end
 end

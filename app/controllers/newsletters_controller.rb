3f720f2edde76339d40c129197d77153f1c829ca
Alabê Duarte <alabeduarte@gmail.com>
2012-06-30 14:03:44 -0300

    menu

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -6,5 +6,11 @@ class NewslettersController < ApplicationController
   
   def index
     @emails = Newsletter.all(User.all_by_score)
+    first_team = FirstTeam.last_of_the_round
+    if first_team
+      @match = first_team.match
+      @predicted_score = first_team.predicted_score
+      @winners_of_round = Newsletter.all(first_team.squad_winners)
+    end
   end
 end

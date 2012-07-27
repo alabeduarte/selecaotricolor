f3a656abe3eef552312432486d3e2fa77e80f03b
Alabê Duarte <alabeduarte@gmail.com>
2012-07-27 19:39:48 -0300

    fetching news

--- app/models/globo_esporte_reader.rb
+++ app/models/globo_esporte_reader.rb
@@ -8,12 +8,53 @@ class GloboEsporteReader
     news = Array.new
     @reader.selector.css('#globo-carrossel-passos .globo-carrossel-passo').first(limit).each do |item|
       if (item)
-        href = item.css('a').map { |link| link['href'] }
-        title = item.css('a').map { |link| link['title'] }
-        news << GloboEsporteNews.new(url: href.first, title: title.first)
+        doc = item.css('a').map { |doc| doc['href'] }
+        news << GloboEsporteNews.new(url: doc.first)
+      end
+    end
+    
+    @reader.selector.css('#globo_destaque_carrossel ul li').first(limit).each do |item|
+      if (item)
+        doc = item.css('a').text
+        news << GloboEsporteNews.new(title: doc)
+      end
+    end
+    
+    @reader.selector.css('#globo-carrossel-thumbs div.globo-carrossel-thumb-content').first(limit).each do |item|
+      if (item)
+        doc = item.css('img').map { |doc| doc['src'] }
+        news << GloboEsporteNews.new(image: doc.first)
       end
     end
     news
+  end
+  
+private
+  def fetch_url(selector, limit)
+    selector.css('#globo-carrossel-passos .globo-carrossel-passo').first(limit).each do |item|
+      if (item)
+        doc = item.css('a').map { |doc| doc['href'] }
+        return doc.first
+      end
+    end
+  end
+  
+  def fetch_text(selector, limit)
+    selector.css('#globo_destaque_carrossel ul li').first(limit).each do |item|
+      if (item)
+        doc = item.css('a').text
+        return doc.first
+      end
+    end
+  end
+  
+  def fetch_image(selector, limit)
+    selector.css('#globo-carrossel-thumbs div.globo-carrossel-thumb-content').first(limit).each do |item|
+      if (item)
+        doc = item.css('img').map { |doc| doc['src'] }
+        return doc.first
+      end
+    end
   end
   
 end

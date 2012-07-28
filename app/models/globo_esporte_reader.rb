9be5dde91d3155610b020b226f7681990572948f
Alabê Duarte <alabeduarte@gmail.com>
2012-07-28 00:18:53 -0300

    add news

--- app/models/globo_esporte_reader.rb
+++ app/models/globo_esporte_reader.rb
@@ -6,55 +6,28 @@ class GloboEsporteReader
   
   def breaking_news(limit)
     news = Array.new
-    @reader.selector.css('#globo-carrossel-passos .globo-carrossel-passo').first(limit).each do |item|
-      if (item)
-        doc = item.css('a').map { |doc| doc['href'] }
-        news << GloboEsporteNews.new(url: doc.first)
-      end
-    end
-    
-    @reader.selector.css('#globo_destaque_carrossel ul li').first(limit).each do |item|
-      if (item)
-        doc = item.css('a').text
-        news << GloboEsporteNews.new(title: doc)
-      end
-    end
-    
-    @reader.selector.css('#globo-carrossel-thumbs div.globo-carrossel-thumb-content').first(limit).each do |item|
-      if (item)
-        doc = item.css('img').map { |doc| doc['src'] }
-        news << GloboEsporteNews.new(image: doc.first)
-      end
+    urls = fetch(limit, '#globo-carrossel-passos .globo-carrossel-passo', 'a', 'href')
+    titles = fetch(limit, '#globo_destaque_carrossel ul li', 'a')
+    images = fetch(limit, '#globo-carrossel-thumbs div.globo-carrossel-thumb-content', 'img', 'src')
+    urls.size.times do |index|
+      news << GloboEsporteNews.new(url: urls[index], title: titles[index], image: images[index])
     end
     news
   end
   
 private
-  def fetch_url(selector, limit)
-    selector.css('#globo-carrossel-passos .globo-carrossel-passo').first(limit).each do |item|
+  def fetch(limit, css, tag, *options)
+    array = Array.new
+    @reader.selector.css(css).first(limit).each do |item|
       if (item)
-        doc = item.css('a').map { |doc| doc['href'] }
-        return doc.first
+        if (options.empty?)
+          array << item.css(tag).text
+        else
+          array << item.css(tag).map { |doc| doc[options.first] }.first
+        end
       end
     end
-  end
-  
-  def fetch_text(selector, limit)
-    selector.css('#globo_destaque_carrossel ul li').first(limit).each do |item|
-      if (item)
-        doc = item.css('a').text
-        return doc.first
-      end
-    end
-  end
-  
-  def fetch_image(selector, limit)
-    selector.css('#globo-carrossel-thumbs div.globo-carrossel-thumb-content').first(limit).each do |item|
-      if (item)
-        doc = item.css('img').map { |doc| doc['src'] }
-        return doc.first
-      end
-    end
+    array
   end
   
 end

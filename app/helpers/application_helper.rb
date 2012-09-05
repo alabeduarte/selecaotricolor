ed227fe64aea727188f8319aa7b73afd734545d1
Alabê Duarte <alabeduarte@gmail.com>
2012-09-05 20:25:55 -0300

    setting vars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  
-  # Shows link with style "current" in case when the target controller is same as 
+
+  # Shows link with style "current" in case when the target controller is same as
   # current.
   def menu_link_to(title, options = {}, html_options = {})
     if current_page?(options)
@@ -10,17 +10,21 @@ module ApplicationHelper
 
     link_to(title, options, html_options)
   end
-  
+
   def current_uri
     "#{request.host}#{request.fullpath}"
   end
-  
+
   def next_match
     Calendar.next_match
   end
-  
+
   def current_user_owner_of?(formation)
     current_user.owner_of? formation unless !user_signed_in?
   end
-  
+
+  def facebook_app_id
+    ENV["FB_APP_ID"]
+  end
+
 end

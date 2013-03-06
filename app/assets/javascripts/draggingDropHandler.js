b6f478389b7b872cc4cad611cfc53ec983b53c8f
Alabê Duarte <alabeduarte@gmail.com>
2013-03-05 21:33:27 -0300

    Init slots as droppable

--- app/assets/javascripts/draggingDropHandler.js
+++ app/assets/javascripts/draggingDropHandler.js
@@ -2,8 +2,22 @@ DraggingDropHandler = function(soccerField) {
   this.soccerField = soccerField;
 }
 
-DraggingDropHandler.prototype.cleanUp = function() {
+DraggingDropHandler.prototype.init = function() {
+  this.initSlots();
+  this.disable($('#send_button'));
+}
+
+DraggingDropHandler.prototype.initSlots = function() {
   $('#slot').html('');
+  for(var i=0; i < this.soccerField.slotSize(); i++) {
+    var item = '<div id=' + i + '></div>';
+    $(item).data('number', i).appendTo('#slot').droppable({});
+  }
+}
+
+DraggingDropHandler.prototype.disable = function(button) {
+  button.attr('disabled', 'disabled');
+	button.addClass("disabled");
 }
 
 
--- spec/javascripts/draggingDropHandlerSpec.js
+++ spec/javascripts/draggingDropHandlerSpec.js
@@ -2,15 +2,23 @@ describe("DraggingDropHandler", function() {
   var draggingDropHandler;
 
   beforeEach(function() {
-    //loadFixtures('draggingDropHandler.html');
+    loadFixtures('draggingDropHandler.html');
     var soccerField = new SoccerField(5, 8);
     draggingDropHandler = new DraggingDropHandler(soccerField);
   });
 
   describe("Initializing soccer field", function() {
-    it("should clean up #slot", function() {
-      draggingDropHandler.cleanUp();
-      expect($('#slot').html()).toEqual('');
+    it("should build each slot as droppable", function() {
+      draggingDropHandler.init();
+      var soccerField = draggingDropHandler.soccerField;
+      for(var i=0; i < soccerField.slotSize(); i++) {
+        var item = $('#' + i);
+        expect(item.hasClass('ui-droppable')).toBe(true);
+      }
+    });
+    it("should disable #send_button", function() {
+      draggingDropHandler.init();
+      expect($('#send_button').hasClass('disabled')).toBe(true);
     });
   });
 

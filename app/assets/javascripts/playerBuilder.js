8c373674be50da9ba5bcb7d62eb89590a28ceea9
Alabê Duarte <alabeduarte@gmail.com>
2013-03-08 16:06:53 -0300

    Adding class for player and goalkeepers

--- app/assets/javascripts/playerBuilder.js
+++ app/assets/javascripts/playerBuilder.js
@@ -12,9 +12,37 @@ PlayerBuilder.prototype.findAll = function() {
   return result;
 }
 
+PlayerBuilder.prototype.getPositionDivName = function(player) {
+  switch(player.position_mapper.code) {
+    case "G":
+      return "#goalkeepers";
+    case "DD":
+      return "#right_back";
+    case "DC":
+      return "#defender";
+    case "DE":
+      return "#left_back";
+    case "MDC":
+      return "#midfield_defense";
+  }
+  switch(player.position_mapper.code.substring(0, 1)) {
+    case "M":
+      return "#midfield";
+    case "A":
+      return "#forwards";
+  }
+}
+
+PlayerBuilder.prototype.isGoalKeeper = function(player) {
+  return player.position_mapper.code === "G";
+}
+
 PlayerBuilder.prototype.create = function(players) {
+  var builder = this;
   $.each(players, function(index, player) {
-    var positionDivName = "#forwards";
+    var positionDivName = builder.getPositionDivName(player);
     $('<div id=' + player.id + ' name=' + positionDivName + '></div>').appendTo(positionDivName);
+    var positionClass = builder.isGoalKeeper(player)? "goal_keeper": "team";
+    $("#" + player.id).addClass(positionClass);
   });
 }
--- spec/javascripts/playerBuilderSpec.js
+++ spec/javascripts/playerBuilderSpec.js
@@ -5,7 +5,7 @@ describe("PlayerBuilder", function() {
   beforeEach(function() {
     loadFixtures('newFormation.html');
     server = sinon.fakeServer.create();
-    server.respondWith("GET", "/bahia_squad.json", [200, { "Content-Type": "application/json" }, '[{ "player": { "id":"playerId","name":"Souza","number":9 } }]']);
+    server.respondWith("GET", "/bahia_squad.json", [200, { "Content-Type": "application/json" }, '[{ "player": { "id":"playerId","name":"Souza","number":9, "position_mapper":{"code":"AC"} } }]']);
 
     playerBuilder = new PlayerBuilder();
   });
@@ -35,16 +35,67 @@ describe("PlayerBuilder", function() {
         expect(player.id).toEqual("playerId");
         expect(player.name).toEqual("Souza");
         expect(player.number).toEqual(9);
+        expect(player.position_mapper.code).toEqual("AC");
       });
     });
   });
 
   describe("Preparing Players", function() {
     describe("Creating div for players", function() {
-      it("should create div for player with player id", function() {
-        var players = [{id: "playerId", name: "Souza", number: "9"}];
-        playerBuilder.create(players);
-        expect($('#forwards')).toContain($('#playerId'));
+      describe("getting position div name", function() {
+        it("should get #goalkeepers", function() {
+          var player = {name: "Marcelo Lomba", position_mapper: {code: "G"}};
+          expect(playerBuilder.getPositionDivName(player)).toEqual("#goalkeepers");
+          expect(playerBuilder.isGoalKeeper(player)).toBe(true);
+        });
+        it("should get #right_back", function() {
+          var player = {name: "Neto", position_mapper: {code: "DD"}};
+          expect(playerBuilder.getPositionDivName(player)).toEqual("#right_back");
+        });
+        it("should get #defender", function() {
+          var player = {name: "Titi", position_mapper: {code: "DC"}};
+          expect(playerBuilder.getPositionDivName(player)).toEqual("#defender");
+        });
+        it("should get #left_back", function() {
+          var player = {name: "Jussandro", position_mapper: {code: "DE"}};
+          expect(playerBuilder.getPositionDivName(player)).toEqual("#left_back");
+        });
+        it("should get #midfield_defense", function() {
+          var player = {name: "Fahel", position_mapper: {code: "MDC"}};
+          expect(playerBuilder.getPositionDivName(player)).toEqual("#midfield_defense");
+        });
+        it("should get #midfield", function() {
+          var player = {name: "Rosales", position_mapper: {code: "ME"}};
+          expect(playerBuilder.getPositionDivName(player)).toEqual("#midfield");
+        });
+        it("should get #midfield", function() {
+          var player = {name: "Souza", position_mapper: {code: "AC"}};
+          expect(playerBuilder.getPositionDivName(player)).toEqual("#forwards");
+        });
+      });
+      describe("Appending to position div", function() {
+        it("should create div for player with player id inside in his position mapper", function() {
+          var players = [{id: "playerId", name: "Neto", number: "2", position_mapper: {code: "DD"}}];
+          var player = players[0];
+          var positionDivName = playerBuilder.getPositionDivName(player);
+          playerBuilder.create(players);
+          expect($(positionDivName)).toContain($('#playerId'));
+        });
+        it("should add .goal_keeper css class for goalkeeper", function() {
+          var players = [{id: "playerId", name: "Lomba", number: "1", position_mapper: {code: "G"}}];
+          var player = players[0];
+          var positionDivName = playerBuilder.getPositionDivName(player);
+          playerBuilder.create(players);
+          expect($("#playerId")).toHaveClass("goal_keeper");
+        });
+        it("should add .team css class for anothers", function() {
+          var players = [{id: "playerId", name: "Fahel", number: "7", position_mapper: {code: "MDC"}}];
+          var player = players[0];
+          var positionDivName = playerBuilder.getPositionDivName(player);
+          playerBuilder.create(players);
+          expect($("#playerId")).toHaveClass("team");
+        });
+
       });
     });
   });

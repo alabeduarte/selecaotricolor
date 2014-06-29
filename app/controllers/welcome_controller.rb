3235cb7bcaa79b29227e260a60a2e9e8d030ec82
Alabê Duarte <alabeduarte@gmail.com>
2014-06-29 14:53:20 -0300

    Removed latest match formation from welcome#index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,12 +3,6 @@ require 'cache'
 class WelcomeController < ApplicationController
 
   def index
-    @next_match = Calendar.next_match
-    @first_team = FirstTeam.last_of_the_round
-    if found_first_team? @first_team
-      @recent_winners = @first_team.squad_winners
-      @formation = @first_team.formation
-    end
   end
 
   def ecbahia_news
